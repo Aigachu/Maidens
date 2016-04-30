b7075846dd049484bc96cb70ee0e4a91e42d03e2
Aigachu <aigachu.sama@gmail.com>
2016-04-30 06:12:38 -0400

    Added the handicap command! :D FUN STREAM YAY HAHAHAHA!

--- classes/SoraClient/Commands.js
+++ classes/SoraClient/Commands.js
@@ -403,7 +403,7 @@ commands.ironman = {
         chosen_characters[random_character] = random_character;
       }
 
-      var message = "Here's your list of iron man characters!\n";
+      var message = "Here's your list of iron man characters!\n\n";
 
       for (var key in charlist) {
         if(charlist.hasOwnProperty(key)) {
@@ -428,7 +428,130 @@ commands.ironman = {
  */
 commands.handicap = {
   fn: function(sora, params, msg) {
-    // code 
+    var handicaps = [];
+
+    handicaps.push({
+      title: "Air Mac",
+      details: "You may not use aerials during this match!",
+      timeout: 3
+    });
+    handicaps.push({
+      title: "FG Link",
+      details: "You may only use Special Moves (B) moves to attack during this match!",
+      timeout: 3
+    });
+    handicaps.push({
+      title: "A for Effort",
+      details: "You may only use Normal Attacks (A) moves to attack during this match!",
+      timeout: 3
+    });
+    handicaps.push({
+      title: "Smashing Effort",
+      details: "You may only attack using smash attacks!",
+      timeout: 3
+    });
+    handicaps.push({
+      title: "Take Flight",
+      details: "You may only attack using aerials!",
+      timeout: 3
+    });
+    handicaps.push({
+      title: "We Tech Those",
+      details: "You may not tech during this match!",
+      timeout: 3
+    });
+    handicaps.push({
+      title: "Dunkin Donuts",
+      details: "You may only finish off stocks with Dunks!",
+      timeout: 3
+    });
+    handicaps.push({
+      title: "SOLOYOLO",
+      details: "You may only kill by taking someone to the shadow realm with you!",
+      timeout: 3
+    });
+    handicaps.push({
+      title: "Best Defense is Offense",
+      details: "You may not shield during this match!",
+      timeout: 3
+    });
+    handicaps.push({
+      title: "Counterattack",
+      details: "You may only kill someone by using a counter move!",
+      timeout: 3
+    });
+    handicaps.push({
+      title: "Manhandled",
+      details: "You may only grab to during this match to attack!",
+      timeout: 3
+    });
+    handicaps.push({
+      title: "One and Done",
+      details: "You may not recover once knocked off stage!",
+      timeout: 3
+    });
+    handicaps.push({
+      title: "Pitch a Tent",
+      details: "You may only win by timing your opponent out!",
+      timeout: 3
+    });
+    handicaps.push({
+      title: "Trigger Happy",
+      details: "You may not use your shoulder buttons (shield buttons) during this match!",
+      timeout: 3
+    });
+    handicaps.push({
+      title: "Slow and Steady",
+      details: "You may only walk in order to move during this match!",
+      timeout: 3
+    });
+    handicaps.push({
+      title: "Get your Footing",
+      details: "You may only end your opponent's stocks with footstools!",
+      timeout: 3
+    });
+    handicaps.push({
+      title: "The Bakery",
+      details: "You may not roll during this match!",
+      timeout: 3
+    });
+    handicaps.push({
+      title: "Iron Boots",
+      details: "You may not jump during this match!",
+      timeout: 3
+    });
+    handicaps.push({
+      title: "Comeback King",
+      details: "You must forfeit your first stock at the beginning of the match!",
+      timeout: 3
+    });
+    handicaps.push({
+      title: "Too Edgy",
+      details: "You may not edgeguard the opponent during the match!",
+      timeout: 3
+    });
+    handicaps.push({
+      title: "On Tilt",
+      details: "You must kill the opponent utilizing a tilt attack!",
+      timeout: 3
+    });
+
+
+    var rand = handicaps[Math.floor(Math.random() * handicaps.length)];
+
+    sora.sendMessage(msg.channel, sora.helpers.printUserTag(msg.author.id) + ", your handicap will be...");
+    sora.startTyping(msg.channel);
+
+    setTimeout(function(){
+      var message = "**" + rand.title + "**";
+
+      message += "\n" + rand.details;
+
+      message += "\n\nGood luck! :P"
+
+      sora.sendMessage(msg.channel, message);
+      sora.stopTyping(msg.channel);
+    }, 1000 * rand.timeout);
   }
 }
 
