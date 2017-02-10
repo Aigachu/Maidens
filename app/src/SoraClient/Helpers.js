689bc71dc225ec7fb1ec15cb72fd8f5e30e6c8db
Aiga Chu <aigachu.sama@gmail.com>
2017-02-10 16:45:52 -0500

    Made some cleanup changes to the code.

--- app/src/SoraClient/Helpers.js
+++ app/src/SoraClient/Helpers.js
@@ -10,81 +10,84 @@
  * Sora likes it when it's clean, so keep it tidy!
  */
 
-/* === Functions Start === */
+class SoraUtil {
 
-/**
- * [val description]
- * @param  {[type]} params [description]
- * @param  {[type]} count  [description]
- * @return {[type]}        [description]
- */
-exports.val = function(params, count) {
-  // If the count parameter isn't provided, we'll default it to 1.
-  if(!count) {
-    count = 1;
+  /* === Functions Start === */
+
+  /**
+   * [val description]
+   * @param  {[type]} params [description]
+   * @param  {[type]} count  [description]
+   * @return {[type]}        [description]
+   */
+  val(params, count) {
+    // If the count parameter isn't provided, we'll default it to 1.
+    if(!count) {
+      count = 1;
+    }
+
+    console.log(params.length)
+
+    if(count == 0) {
+      var someshit = (params.length <= 0) ? true : false;
+      console.log(someshit);
+      return (params.length <= 0) ? true : false;
+    } else {
+      return (params[count]) ? true : false;
+    }
+  };
+
+  /**
+   * Implements the custom function: extractID
+   * @param  {[type]} tag [A discord string representing a user's Tag (highlighted string).]
+   * @return {[type]}     [ID from the tag string.]
+   */
+ extractID(tag) {
+    return tag.slice(2, -1);
+  };
+
+  /**
+   * [extractParam description]
+   * @param  {[type]} start [description]
+   * @param  {[type]} end   [description]
+   * @param  {[type]} msg   [description]
+   * @return {[type]}       [description]
+   */
+  extractParam(start, end, msg) {
+    start = msg.content.indexOf(start);
+    end = msg.content.indexOf(end);
+
+    return msg.content.substring(start + 1, end);
   }
 
-  console.log(params.length)
+  /**
+   * Implements the custom function: printUserTag
+   * @param  {[object/string]} variable [Can be either a user object]
+   * @return {[string]}                 [String that will be interpreted by discord to tag user. i.e. "<@77517077325287424>"]
+   */
+  printUserTag(variable) {
+    // Conditional check to verify if the variable is an object (user) or not.
+    if(variable.username) {
+      return "<@" + variable.id + ">";
+    } else {
+      return "<@" + variable + ">";
+    }
+  };
 
-  if(count == 0) {
-    var someshit = (params.length <= 0) ? true : false;
-    console.log(someshit);
-    return (params.length <= 0) ? true : false;
-  } else {
-    return (params[count]) ? true : false;
+  /**
+   * [removeCooldown description]
+   * @param  {[type]} key [description]
+   * @return {[type]}     [description]
+   */
+  removeCooldown(client, key) {
+    if(typeof client.commands_properties[key] !== 'undefined') {
+      setTimeout(function(){ client.cooldowns[key] = false; console.log("Removed cooldown for " + key); }, 1000 * client.commands_properties[key].cooldown);
+    } else {
+      setTimeout(function(){ client.cooldowns[key] = false; console.log("Removed cooldown for " + key); }, 1000 * 15);
+    }
   }
-};
-
-/**
- * Implements the custom function: extractID
- * @param  {[type]} tag [A discord string representing a user's Tag (highlighted string).]
- * @return {[type]}     [ID from the tag string.]
- */
-exports.extractID = function(tag) {
-  return tag.slice(2, -1);
-};
-
-/**
- * [extractParam description]
- * @param  {[type]} start [description]
- * @param  {[type]} end   [description]
- * @param  {[type]} msg   [description]
- * @return {[type]}       [description]
- */
-exports.extractParam = function(start, end, msg) {
-  start = msg.content.indexOf(start);
-  end = msg.content.indexOf(end);
-
-  return msg.content.substring(start + 1, end);
 }
 
-/**
- * Implements the custom function: printUserTag
- * @param  {[object/string]} variable [Can be either a user object]
- * @return {[string]}                 [String that will be interpreted by discord to tag user. i.e. "<@77517077325287424>"]
- */
-exports.printUserTag = function(variable) {
-  // Conditional check to verify if the variable is an object (user) or not.
-  if(variable.username) {
-    return "<@" + variable.id + ">";
-  } else {
-    return "<@" + variable + ">";
-  }
-};
-
-/**
- * [removeCooldown description]
- * @param  {[type]} key [description]
- * @return {[type]}     [description]
- */
-exports.removeCooldown = function(key) {
-  if(typeof sora.commands_properties[key] !== 'undefined') {
-    setTimeout(function(){ sora.cooldowns[key] = false; console.log("Removed cooldown for " + key); }, 1000 * sora.commands_properties[key].cooldown);
-  } else {
-    setTimeout(function(){ sora.cooldowns[key] = false; console.log("Removed cooldown for " + key); }, 1000 * 15);
-  }
-}
-
-/* === Functions End === */
+module.exports = SoraUtil;
 
 
