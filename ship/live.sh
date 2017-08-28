444ad97f791e13197e004652f889aa0a6e1ae9a5
Aiga Chu <aigachu.sama@gmail.com>
2017-08-28 12:12:34 -0400

    Hotfix - Changed the order of actions in the deploy script.

--- ship/live.sh
+++ ship/live.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 #
 # Build instructions for live environnement
+# 
+
+# Stop any bot processes that may currently be running.
+ssh aigachu@138.197.174.254 'forever stopall;'
 
 # Clone repository if not present.
 # Generate ssh key for aigachu user and put it in github.
 ssh aigachu@138.197.174.254 'cd nodejs/apps; git clone git@github.com:Aigachu/discord-maidens.git'
-
-# Make sure dependencies are installed.
-ssh aigachu@138.197.174.254 'cd nodejs/apps/discord-maidens/node; npm install;'
-
-# Alert for settings.
-# DO NOT track settings for each bot.
-# Different environnements will have different settings and because github is public,
-# people will have access to their tokens. You do NOT want that to happen!
-ssh aigachu@138.197.174.254 'echo "Make sure settings.js is set for all maidens."'
-
-# Stop any bot processes that may currently be running.
-ssh aigachu@138.197.174.254 'forever stopall;'
 
 # Prune origin first
 ssh aigachu@138.197.174.254 'cd nodejs/apps/discord-maidens/node; git remote prune origin;'
@@ -26,6 +18,15 @@ ssh aigachu@138.197.174.254 'cd nodejs/apps/discord-maidens/node; git checkout l
 
 # Pull latest changes.
 ssh aigachu@138.197.174.254 'cd nodejs/apps/discord-maidens/node; git pull;'
+
+# Make sure dependencies are installed.
+ssh aigachu@138.197.174.254 'cd nodejs/apps/discord-maidens/node; npm install;'
+
+# Alert for settings.
+# DO NOT track settings for each bot.
+# Different environnements will have different settings and because github is public,
+# people will have access to their tokens. You do NOT want that to happen!
+ssh aigachu@138.197.174.254 'echo "Make sure settings.js is set for all maidens."'
 
 # Summon the bots.
 ssh aigachu@138.197.174.254 'cd nodejs/apps/discord-maidens/node; forever start summon.js;'
