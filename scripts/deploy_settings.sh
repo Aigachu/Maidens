17d14b0e32118e24982373504ca797a163d3674d
Aiga Chu <aigachu.sama@gmail.com>
2017-09-05 16:17:28 -0400

    Made deploy_settings script restart bots.

--- scripts/deploy_settings.sh
+++ scripts/deploy_settings.sh
@@ -7,3 +7,6 @@ for maiden in ../node/app/maidens/*/ ; do
 	# Deploy Settings.
 	scp -r ../node/app/maidens/$m/settings.js aigachu@138.197.174.254:~/nodejs/apps/discord-maidens/node/app/maidens/$m/settings.js
 done
+
+ssh aigachu@138.197.174.254 'forever stopall;'
+ssh aigachu@138.197.174.254 'cd nodejs/apps/discord-maidens/node; forever start summon.js;'
