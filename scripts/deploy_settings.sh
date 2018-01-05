8f18e871775bb9fc8c9a18b1677c738b623cc782
Aiga Chu <aigachu.sama@gmail.com>
2018-01-05 01:37:49 -0500

    Updated scripts to fetch and deploy core settings file.

--- scripts/deploy_settings.sh
+++ scripts/deploy_settings.sh
@@ -2,6 +2,8 @@
 #
 # Copy settings for all maidens to the live environnement.
 
+scp -r ../node/core-settings.js aigachu@138.197.174.254:~/nodejs/apps/discord-maidens/node/core-settings.js
+
 for maiden in ../node/app/maidens/*/ ; do
 	m=$(basename ${maiden})
 	echo "Deploying settings for ${m}..."
--- scripts/fetch_settings.sh
+++ scripts/fetch_settings.sh
@@ -2,6 +2,8 @@
 #
 # Copy settings for all maidens to the live environnement.
 
+scp -r aigachu@138.197.174.254:~/nodejs/apps/discord-maidens/node/core-settings.js ../node/core-settings.js
+
 for maiden in ../node/app/maidens/*/ ; do
 	m=$(basename ${maiden})
 	echo "Fetching settings for ${m}..."
