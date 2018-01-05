7940ba86da266f74ce7f6a10b15c61303e863e99
Aigachu <aigachu.sama@gmail.com>
2018-01-05 03:14:08 -0500

    Added checkers to prevent unwanted array splicing.

--- node/app/core/maidendiscord/plugins/twitch/Twitch.js
+++ node/app/core/maidendiscord/plugins/twitch/Twitch.js
@@ -10,109 +10,119 @@
  *
  */
 class Twitch {
-    
+
     constructor(client) {
-        
+
         // The Maiden client.
         this.client = client;
-        
+
         // Role and configuration setup
         this.build();
-        
+
         // Set listener.
         this.listen();
-    
+
         // Set the pinger.
         // The pinger is basically a function that will run every *minute* to check if a stream must be
         // fired. This is intensive, I know, but it's the best (first) way I thought of doing this.
         setInterval(() => {
             this.ping();
-        }, 60000);
-        
+        }, 10000);
+
     }
-    
+
     /**
      *
      */
     ping() {
-        this.client.guilds.every((guild) =>{
-           // TTVAnn
-           if (this.guilds[guild.id].ttvann.enabled) {
-               // If there is no announcement channel set for this guild, we do nothing.
-               if (this.guilds[guild.id].ttvann.ann_channel === null)
-                   return true;
-               
-               // If there are no streams to announce, we do nothing.
-               if (_.isEmpty(this.guilds[guild.id].ttvann.streams))
-                   return true;
-    
-               // If we pass all the checks, we run ttvann.
-               this.ttvann(this.guilds[guild.id].ttvann);
-           }
-           return true;
+        this.client.guilds.every((guild) => {
+            // TTVAnn
+            if (this.guilds[guild.id].ttvann.enabled) {
+                // If there is no announcement channel set for this guild, we do nothing.
+                if (this.guilds[guild.id].ttvann.ann_channel === null)
+                    return true;
+
+                // If there are no streams to announce, we do nothing.
+                if (_.isEmpty(this.guilds[guild.id].ttvann.streams))
+                    return true;
+
+                // If we pass all the checks, we run ttvann.
+                this.ttvann(this.guilds[guild.id].ttvann);
+            }
+            return true;
         });
     }
-    
+
     /**
      *
      * @param guild
      */
     ttvann(guild) {
         guild.streams.every((stream_user) => {
+            console.log(stream_user);
             this.getUserStream(stream_user).then((data) => {
-                if (data.stream !== null)  {
-                    this.ttvannFire(guild, data.stream, stream_user)
+                if (data.stream !== null) {
+                    this.ttvannFire(guild, data.stream, stream_user);
                 } else {
                     this.ttvannRemoveStreamLive(guild.id, stream_user);
                     this.save();
                 }
-               return true;
+                return true;
             });
             return true;
         });
     }
-    
+
     /**
      *
      * @param guild
      * @param stream_data
+     * @param stream_user
      */
     ttvannFire(guild, stream_data, stream_user) {
-        
+
         if (guild.live.indexOf(stream_user) > -1)
             return;
-        
+
         let announcement_channel = this.client.channels.find('id', guild.ann_channel);
-        
+
         let name = stream_data.channel.display_name;
         let game = stream_data.game;
         let url = stream_data.channel.url;
-        
+
         announcement_channel.send(`Hey @here! ${name} is streaming ${game} on Twitch! Come take a look! :eyes: ${url}`);
         this.ttvannAddStreamLive(guild.id, stream_user);
         this.save();
     }
-    
+
     /**
      *
      * @param guild_id
      * @param stream_user
      */
     ttvannAddStream(guild_id, stream_user) {
+        if (this.guilds[guild_id].ttvann.streams.indexOf(stream_user) > -1) {
+            return false;
+        }
         this.guilds[guild_id].ttvann.streams.push(stream_user);
         this.save();
+        return true;
     }
-    
+
     /**
      *
      * @param guild_id
      * @param stream_user
      */
     ttvannAddStreamLive(guild_id, stream_user) {
+        if (this.guilds[guild_id].ttvann.live.indexOf(stream_user) > -1) {
+            return false;
+        }
         this.guilds[guild_id].ttvann.live.push(stream_user);
         this.save();
+        return true;
     }
-    
+
     /**
      *
      * @param guild_id
@@ -122,28 +132,39 @@ class Twitch {
         this.guilds[guild_id].ttvann.ann_channel = channel_id;
         this.save();
     }
-    
+
     /**
      *
      * @param guild_id
      * @param stream_user
      */
     ttvannRemoveStream(guild_id, stream_user) {
-        this.guilds[guild_id].ttvann.streams.splice(this.guilds[guild_id].ttvann.streams.indexOf(stream_user), 1);
-        this.guilds[guild_id].ttvann.live.splice(this.guilds[guild_id].ttvann.live.indexOf(stream_user), 1);
+        let streams_index = this.guilds[guild_id].ttvann.streams.indexOf(stream_user);
+        let live_index = this.guilds[guild_id].ttvann.live.indexOf(stream_user);
+
+        if (streams_index > -1)
+            this.guilds[guild_id].ttvann.streams.splice(streams_index, 1);
+
+        if (live_index > -1)
+            this.guilds[guild_id].ttvann.live.splice(live_index, 1);
+
         this.save();
     }
-    
+
     /**
      *
      * @param guild_id
      * @param stream_user
      */
     ttvannRemoveStreamLive(guild_id, stream_user) {
-        this.guilds[guild_id].ttvann.live.splice(this.guilds[guild_id].ttvann.live.indexOf(stream_user), 1);
+        let live_index = this.guilds[guild_id].ttvann.live.indexOf(stream_user);
+
+        if (live_index > -1)
+            this.guilds[guild_id].ttvann.live.splice(live_index, 1);
+
         this.save();
     }
-    
+
     /**
      * Listener for the Watchdog plugin.
      * This listener will determine what the Watchdog will do upon receiving messages.
@@ -151,21 +172,21 @@ class Twitch {
     listen() {
         // We need to add a ready around this, or else the client will not be set when we try to push the listener.
         this.client.on('ready', () => {
-            
+
             // Add listener to client.
             this.client.listeners.push({
                 // Push the listener's listen function.
                 listen: (client, message) => {
-                    
+
                     // Add listener functionality here...
                 },
-                
+
             });
-            
-            
+
+
         });
     }
-    
+
     /**
      * Enable the Watchdog in a given guild.
      * @param feature
@@ -176,7 +197,7 @@ class Twitch {
         this.save();
         return true;
     }
-    
+
     /**
      * Disable the Watchdog in a given guild.
      * @param feature
@@ -187,7 +208,7 @@ class Twitch {
         this.save();
         return false;
     }
-    
+
     /**
      * Get the status of the Watchdog in a given guild.
      * @param feature
@@ -196,19 +217,19 @@ class Twitch {
     status(feature, guild) {
         return this.guilds[guild.id][feature].enabled;
     }
-    
+
     /**
      * Save configurations.
      */
     save() {
         fs.writeFileSync(this.config_path, JSON.stringify(this.guilds, null, 2));
     }
-    
+
     /**
      * Build core Watchdog necessities.
      */
     build() {
-        
+
         // A ready container is needed her since this is done on construction.
         // We can only manipulate guilds after the client is ready.
         this.client.on('ready', () => {
@@ -217,25 +238,25 @@ class Twitch {
             if (!fs.existsSync(config_dir)) {
                 fs.mkdirSync(config_dir);
             }
-    
+
             // Get path to the appropriate configuration directory or make it if it
             // doesn't exist.
             let desired_config_dir = this.client.coreroot + 'plugins/twitch/config/' + this.client.maiden_name;
             if (!fs.existsSync(desired_config_dir)) {
                 fs.mkdirSync(desired_config_dir);
             }
-    
+
             // Store path to the configuration in the Watchdog object.
             this.config_path = desired_config_dir + '/guilds.json';
-    
+
             // Build Configurations and store them in the Watchdog.
             this.guilds = {};
-    
+
             // If the configurations already exist, load them.
             if (fs.existsSync(this.config_path)) {
                 this.guilds = JSON.parse(fs.readFileSync(this.config_path));
             }
-    
+
             // If the configurations were loaded, we'll stop here.
             if (!_.isEmpty(this.guilds)) {
                 console.log('Maiden Twitch: Loaded guild config from files.');
@@ -255,12 +276,12 @@ class Twitch {
                     return true;
                 });
             }
-    
+
             // Save guild configurations.
             this.save();
         });
     }
-    
+
     /**
      *
      * @param name
@@ -276,7 +297,7 @@ class Twitch {
             });
         });
     }
-    
+
     /**
      *
      * @param user
@@ -285,14 +306,14 @@ class Twitch {
         return new Promise((resolve, reject) => {
             if (typeof user !== 'object') {
                 this.getUserByName(user).then((result) => {
-                   resolve(this.getUserStream(result));
+                    resolve(this.getUserStream(result));
                 });
             } else {
                 resolve(this.getStreamData(user._id));
             }
         });
     }
-    
+
     /**
      *
      * @param id
